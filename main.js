cd4e7a1e60ba455a3f451aebff33a7c1f2be11f1
Shreya <87652405+ShreyaSudheer@users.noreply.github.com>
2022-03-10 19:23:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+noseX = 0;
+noseY = 0;
+
+function preload()
+{
+   
+}
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function draw()
+{
+    image(video, 0, 0, 300,300);
+    fill(255,0,0);
+    stroke(255, 0 ,0);
+    circle(noseX, noseY, 20);
+}
+function modelLoaded()
+{
+    console.log("The PoseNet Model is Loaded");
+}
+function take_snapshot()
+{
+    save("My_Snapshot.png");
+}
+function gotPoses(results)
+{
+    console.log(results);
+    noseX =  results[0].pose.nose.x;
+    console.log("nose X = " + results[0].pose.nose.x);
+    noseY =  results[0].pose.nose.y;
+    console.log("nose Y = " + results[0].pose.nose.y);
+}
